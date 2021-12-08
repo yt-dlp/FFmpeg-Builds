d817fab82ec719a09bb69a8d86f38e597756c158
nihil-admirari <50202386+nihil-admirari@users.noreply.github.com>
2021-12-08 17:25:34 +0300

    Revert "Change sourceforge links"
    This reverts commit 785a1133667f765e2007726840f9d9b8c2364613.

--- scripts.d/25-freetype.sh
+++ scripts.d/25-freetype.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# https://www.freetype.org/download.html
-FREETYPE_SRC="https://download.savannah.gnu.org/releases/freetype/freetype-2.11.0.tar.xz"
+# https://sourceforge.net/projects/freetype/files/freetype2/
+FREETYPE_SRC="https://sourceforge.net/projects/freetype/files/freetype2/2.11.1/freetype-2.11.1.tar.xz/download"
 
 ffbuild_enabled() {
     return 0
